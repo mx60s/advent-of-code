3a821fd603485b586776a09ad8469379058da4ca
mx60s <vonebersm@gmail.com>
2024-12-08 01:57:08 -0600

    done

--- 4/solve.py
+++ 4/solve.py
@@ -2,28 +2,25 @@ import sys
 import re
 
 def solve_crossword(crossword):
-    def in_bounds(x, y):
-        return 0 <= x < rows and 0 <= y < cols
-
     word = 'XMAS'
 
     def check_direction(x, y, dx, dy):
         for i in range(len(word)):
             nx, ny = x + i * dx, y + i * dy
-            if not in_bounds(nx, ny) or crossword[nx][ny] != word[i]:
+            if not (0 <= x < rows and 0 <= y < cols) or crossword[nx][ny] != word[i]:
                 return False
         return True
 
     rows, cols = len(crossword), len(crossword[0])
     directions = [
-        (0, 1),  # Right
-        (1, 0),  # Down
-        (1, 1),  # Diagonal Down-Right
-        (1, -1), # Diagonal Down-Left
-        (0, -1), # Left
-        (-1, 0), # Up
-        (-1, -1),# Diagonal Up-Left
-        (-1, 1)  # Diagonal Up-Right
+        (0, 1), 
+        (1, 0),
+        (1, 1),
+        (1, -1),
+        (0, -1),
+        (-1, 0),
+        (-1, -1),
+        (-1, 1)
     ]
 
     count = 0
